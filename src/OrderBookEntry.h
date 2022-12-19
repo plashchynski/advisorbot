996edec71a3c41c48944f001cbcd917c9c743f79
Dzmitry Plashchynski <plashchynski@gmail.com>
2022-12-20 00:12:04 +0300

    Clean

--- src/OrderBookEntry.h
+++ src/OrderBookEntry.h
@@ -2,7 +2,7 @@
 
 #include <string>
 
-enum class OrderBookType{bid, ask, unknown, asksale, bidsale};
+enum class OrderBookType{bid, ask, unknown};
 
 class OrderBookEntry
 {
@@ -15,19 +15,6 @@ class OrderBookEntry
                         std::string username = "dataset");
 
         static OrderBookType stringToOrderBookType(std::string s);
-
-        static bool compareByTimestamp(OrderBookEntry& e1, OrderBookEntry& e2)
-        {
-            return e1.timestamp < e2.timestamp;
-        }  
-        static bool compareByPriceAsc(OrderBookEntry& e1, OrderBookEntry& e2)
-        {
-            return e1.price < e2.price;
-        }
-         static bool compareByPriceDesc(OrderBookEntry& e1, OrderBookEntry& e2)
-        {
-            return e1.price > e2.price;
-        }
 
         double price;
         double amount;
