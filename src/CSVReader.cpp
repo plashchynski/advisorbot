2ac60ee134fe5aece6a1658907a9aebbb2c2d2e4
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-01-05 23:23:39 +0300

    Back the original method

--- src/CSVReader.cpp
+++ src/CSVReader.cpp
@@ -2,9 +2,6 @@
 #include <iostream>
 #include <fstream>
 
-#include <boost/algorithm/string.hpp>
-
-
 CSVReader::CSVReader()
 {
 
@@ -21,20 +18,17 @@ std::vector<OrderBookEntry> CSVReader::readCSV(std::string csvFilename)
         while(std::getline(csvFile, line))
         {
             try {
-                std::vector<std::string> tokens;
-                boost::split(tokens, line, [](char c){return c == ',';});
-
-                OrderBookEntry obe = stringsToOBE(tokens);
+                OrderBookEntry obe = stringsToOBE(tokenise(line, ','));
                 entries.push_back(obe);
             }catch(const std::exception& e)
             {
                 std::cout << "CSVReader::readCSV bad data"  << std::endl;
             }
         }
-    }    
+    }
 
     std::cout << "CSVReader::readCSV read " << entries.size() << " entries"  << std::endl;
-    return entries; 
+    return entries;
 }
 
 OrderBookEntry CSVReader::stringsToOBE(std::vector<std::string> tokens)
@@ -52,15 +46,33 @@ OrderBookEntry CSVReader::stringsToOBE(std::vector<std::string> tokens)
          amount = std::stod(tokens[4]);
     }catch(const std::exception& e){
         std::cout << "CSVReader::stringsToOBE Bad float! " << tokens[3]<< std::endl;
-        std::cout << "CSVReader::stringsToOBE Bad float! " << tokens[4]<< std::endl; 
-        throw;        
+        std::cout << "CSVReader::stringsToOBE Bad float! " << tokens[4]<< std::endl;
+        throw;
     }
 
-    OrderBookEntry obe{price, 
-                        amount, 
+    OrderBookEntry obe{price,
+                        amount,
                         tokens[0],
-                        tokens[1], 
+                        tokens[1],
                         OrderBookEntry::stringToOrderBookType(tokens[2])};
 
-    return obe; 
+    return obe;
+}
+
+std::vector<std::string> CSVReader::tokenise(std::string csvLine, char separator)
+{
+   std::vector<std::string> tokens;
+   signed int start, end;
+   std::string token;
+    start = csvLine.find_first_not_of(separator, 0);
+    do{
+        end = csvLine.find_first_of(separator, start);
+        if (start == csvLine.length() || start == end) break;
+        if (end >= 0) token = csvLine.substr(start, end - start);
+        else token = csvLine.substr(start, csvLine.length() - start);
+        tokens.push_back(token);
+    start = end + 1;
+    }while(end > 0);
+
+   return tokens;
 }
--- src/CSVReader.h
+++ src/CSVReader.h
@@ -9,7 +9,6 @@ class CSVReader
     public:
         CSVReader();
         static std::vector<OrderBookEntry> readCSV(std::string csvFile);
-
-    private:
         static OrderBookEntry stringsToOBE(std::vector<std::string> strings);
+        static std::vector<std::string> tokenise(std::string csvLine, char separator);
 };
